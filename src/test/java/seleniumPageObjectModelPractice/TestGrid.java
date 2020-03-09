4f5d70859c56615fa7575304d345fcf3a35f130f
TetianaPl <tetiana.plaksina@gmail.com>
2020-03-09 19:41:19 +0200

    Grid

---
+++ src/test/java/seleniumPageObjectModelPractice/TestGrid.java
@@ -0,0 +1,47 @@
+package seleniumPageObjectModelPractice;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestGrid {
+
+    WebDriver driver;
+    String baseURL, nodeURL;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        baseURL = "http://demo.guru99.com/test/guru99home/";
+        nodeURL = "http://192.168.0.101:23894/wd/hub";
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        capability.setBrowserName("chrome");
+        capability.setPlatform(Platform.WINDOWS);
+        driver = new RemoteWebDriver(new URL(nodeURL), capability);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+
+    @Test
+    public void sampleTest() {
+        driver.get(baseURL);
+
+
+        if (driver.getPageSource().contains("MOBILE TESTING")) {
+            Assert.assertTrue(true, "Mobile Testing Link Found");
+        } else {
+            Assert.assertTrue(false, "Failed: Link not found");
+        }
+
+    }
+
+}
+
